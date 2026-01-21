f33ffe6ae66e5d84abf32affa33327231db5fe3f
suyogly <bizpurpose09@gmail.com>
2026-01-21 15:44:45 +0545

    feat: api for comparing two tokenizers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
 from token_counter.tokenize_tiktoken import get_tokens
+from token_counter.tokenize_wordpiece import wordpiece
 
 app = FastAPI()
 
@@ -7,7 +8,14 @@ app = FastAPI()
 def tiktoken_tokens(payload : dict = Body(...)):
    text = payload.get("input", "")
    return get_tokens(payload=text)
-    
 
-if __name__ == "__main__":
-    tiktoken_tokens()
+@app.post("/wordpiece")
+def wordpiece_tokens(payload: dict = Body(...)):
+    text = payload.get("input", "")
+    return wordpiece(payload=text)
+    
+@app.post("/compare-tiktoken-wordpiece")
+def compare_tiktoken_wordpiece(payload: dict = Body(...)):
+   return tiktoken_tokens(payload=payload), wordpiece_tokens(payload=payload)
+
+  
