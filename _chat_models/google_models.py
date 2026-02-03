d2f6313351e9d3b02e5b4e382100e8577415afb2
suyogly <bizpurpose09@gmail.com>
2026-02-03 15:52:11 +0545

    changed names and deleted stuff

--- _chat_models/google_models.py
+++
@@ -1,5 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-def gemini_flash_2_5(llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)):
-    return llm
-
--- _chat_models/groq_models.py
+++
@@ -1,4 +0,0 @@
-from langchain_groq import ChatGroq
-
-def gpt_oss_120b(llm=ChatGroq(model="openai/gpt-oss-120b", temperature=0.7)):
-    return llm
